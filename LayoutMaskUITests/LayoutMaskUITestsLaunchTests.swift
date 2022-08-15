178c55e751e9b51163b26ab599af671449c54cb6
ice.chen <ice.chen@gogoro.com>
2022-08-15 17:25:05 +0800

    Initial Commit

---
+++ LayoutMaskUITests/LayoutMaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LayoutMaskUITestsLaunchTests.swift
+//  LayoutMaskUITests
+//
+//  Created by Ice Chen 陳炳璋 on 2022/8/15.
+//
+
+import XCTest
+
+class LayoutMaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
